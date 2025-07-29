a3d6e5c2489da7983bc67d075a9758366e5c38e5
saramotaha <saramohamedtaha909@gmail.com>
2025-07-29 19:00:20 +0300

    routes error fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,14 +20,14 @@ import { TodayBookingsComponent } from './components/host/pt2/components/today-b
 import { Reservations } from './components/host/pt2/components/reservations/reservations';
 import { Violations } from './components/host/pt2/components/violations/violations';
 
-import { Checkout } from './Pages/checkout/checkout';
-import { Messages } from './User/messages/messages';
-import { Notifications } from './Pages/notifications/notifications';
-import { Profile } from './Pages/profile/profile';
-import { PropertyPhotos } from './Pages/property-photos/property-photos';
-import { Trips } from './Pages/trips/trips';
-import { Wishlist } from './Pages/wishlist/wishlist';
-import { ProfileInfo } from './Pages/profile-info/profile-info';
+// import { Checkout } from './Pages/checkout/checkout';
+// import { Messages } from './User/messages/messages';
+// import { Notifications } from './Pages/notifications/notifications';
+// import { Profile } from './Pages/profile/profile';
+// import { PropertyPhotos } from './Pages/property-photos/property-photos';
+// import { Trips } from './Pages/trips/trips';
+// import { Wishlist } from './Pages/wishlist/wishlist';
+// import { ProfileInfo } from './Pages/profile-info/profile-info';
 import { NotFound } from './Pages/not-found/not-found';
 import { Favorites } from './User/favorites/favorites';
 
@@ -41,9 +41,7 @@ import { Earnings } from './components/host/pt2/components/earnings/earnings';
 import { PropertyListComponent } from './components/host/pt1/property-list/property-list';
 import { listingsRoutes } from './components/host/pt1/listings/listings.routes';
 import { ListingStep1Component } from './components/host/pt1/listings/listing-step1/listing-step1.component';
-import { Messages
-
- } from './components/host/pt2/components/messages/messages';
+// import { Messages } from './components/host/pt2/components/messages/messages';
 import { PropertyManagementComponent } from './Admin/Component/admin-properties-manegment/admin-property-management/admin-property-management';import { ViewAllHomes } from './components/view-all-homes/view-all-homes';
 import { Login } from './Pages/login/login';
 import { AdminPayment } from './Admin/Component/admin-payment/admin-payment';
@@ -88,21 +86,21 @@ export const routes: Routes = [
   { path: 'host/earnings', component: Earnings},
   { path: 'host/listings', component: PropertyListComponent},
   { path: 'host/create-listing', component: ListingStep1Component},
-  { path: 'host/messages', component: Messages},
+  // { path: 'host/messages', component: Messages},
   // { path: "**", component: NotFound} //MUST BE AT THE END
   { path: '', component: PropertyListComponent },
   { path: 'host', children: listingsRoutes },
 
-    { path: "messages", component: Messages , pathMatch:"full" },
+    // { path: "messages", component: Messages , pathMatch:"full" },
   { path: "favorites",component:Favorites,pathMatch:"full"},
-  { path: "notifications", component: Notifications , pathMatch:"full" },
+  // { path: "notifications", component: Notifications , pathMatch:"full" },
   // { path: "AdminNotifications", component: DashBoardBar , pathMatch:"full" },
 
-  { path: "propertyPhotos", component: PropertyPhotos , pathMatch:"full" },
-  { path: "trips", component: Trips , pathMatch:"full" },
-  { path: "wishList", component: Wishlist , pathMatch:"full" },
-  { path: "profile", component: Profile , pathMatch:"full" },
-  { path: "profileInfo", component: ProfileInfo , pathMatch:"full" },
-  { path: "checkout", component: Checkout , pathMatch:"full" },
+  // { path: "propertyPhotos", component: PropertyPhotos , pathMatch:"full" },
+  // { path: "trips", component: Trips , pathMatch:"full" },
+  // { path: "wishList", component: Wishlist , pathMatch:"full" },
+  // { path: "profile", component: Profile , pathMatch:"full" },
+  // { path: "profileInfo", component: ProfileInfo , pathMatch:"full" },
+  // { path: "checkout", component: Checkout , pathMatch:"full" },
   { path: "**", component: NotFound}
 ];
